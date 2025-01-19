cf53da6600accf2688e6b5bb60f0ad0c30e8e8e7
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-01-19 22:56:16 +0530

    Time: 53 ms (44.85%), Space: 46.7 MB (92.01%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        List<Set<Integer>> adj = new ArrayList<>();
+        List<Set<Integer>> isPrereq = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for(int i = 0; i < numCourses; i++) {
+            adj.add(new HashSet<>());
+            isPrereq.add(new HashSet<>());
+        }
+
+        for(int[] pre : prerequisites) {
+            adj.get(pre[0]).add(pre[1]);
+            indegree[pre[1]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0 ){
+                queue.offer(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            for(int neighbor : adj.get(node)) { 
+                isPrereq.get(neighbor).add(node);
+                isPrereq.get(neighbor).addAll(isPrereq.get(node));
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for(int[] query : queries) {
+            result.add(isPrereq.get(query[1]).contains(query[0]));
+        }
+
+        return result;
+    }
+}
