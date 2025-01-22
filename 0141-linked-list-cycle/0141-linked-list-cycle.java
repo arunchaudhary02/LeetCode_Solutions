66d1c6135c36e12688dbe764fa04c320f369654d
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-01-22 14:31:28 +0530

    Time: 0 ms (100%), Space: 44.4 MB (75.13%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,16 +9,40 @@
  *     }
  * }
  */
+
+// HashSet
+
+
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         Set<ListNode> seen = new HashSet<>();
+//         ListNode curr = head;
+//         while(curr != null) {
+//             if(seen.contains(curr)) {
+//                 return true;
+//             }
+//             seen.add(curr);
+//             curr = curr.next;
+//         }
+
+//         return false;
+//     }
+// }
+
+// second method (Tortoise and Hare Algorithm)
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> seen = new HashSet<>();
-        ListNode curr = head;
-        while(curr != null) {
-            if(seen.contains(curr)) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) {
                 return true;
             }
-            seen.add(curr);
-            curr = curr.next;
         }
 
         return false;
