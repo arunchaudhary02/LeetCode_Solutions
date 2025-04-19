bfaa15ab2b99744268ef11f1140d48b79e01f064
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-04-19 20:01:36 +0530

    Time: 24 ms (75.73%), Space: 47 MB (19.7%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,63 @@
+class Solution {
+    // Using Sliding Window
+    public int subarraySum(int[] nums, int k) {
+
+        int n = nums.length;
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+
+        preSumMap.put(0, 1); 
+
+        int sum = 0;
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+
+            int rem = sum - k;
+
+            if(preSumMap.containsKey(rem)) {
+                count += preSumMap.get(rem);
+            }
+
+            preSumMap.put(sum, preSumMap.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+        
+    }
+}
+
+
+// Sliding Window For only positive number in an array
+
+// class Solution {
+//     // Using Sliding Window
+//     public int subarraySum(int[] nums, int k) {
+
+//         int count = 0;
+//         int left = 0;
+//         int right  = 0;
+//         int sum = nums[0];
+
+        
+//         while(right < n) {
+
+//             while(left <= right && sum > k) {
+//                 sum -= nums[left];
+//                 left++;
+//             }
+
+//             if(sum == k) {
+//                 count++;
+//             }
+
+//             right++;
+//             if(right < n) {
+//                 sum += nums[right];
+//             }
+//         }
+
+//         return count;
+        
+//     }
+// }
