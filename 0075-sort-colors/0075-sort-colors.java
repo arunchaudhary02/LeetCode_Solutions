f4dfc61c670b3b7c75ea5dae8b55448a59bc7e4c
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-04-19 22:31:39 +0530

    Time: 0 ms (100%), Space: 42 MB (46.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,30 +1,63 @@
+// class Solution {
+//     public void sortColors(int[] nums) {
+        
+//         int count0 = 0;
+//         int count1 = 0;
+
+//         for(int num : nums) {
+//             if(num == 0) {
+//                 count0++;
+//             }
+//             else if(num == 1) {
+//                 count1++;
+//             }
+            
+//         }
+
+//         for(int i = 0; i < nums.length; i++) {
+//             if(count0 != 0) {
+//                 nums[i] = 0;
+//                 count0--;
+//             }
+//             else if(count1 != 0) {
+//                 nums[i] = 1;
+//                 count1--;
+//             }
+//             else{
+//                 nums[i] = 2;
+//             }
+//         }
+//     }
+
+// }
+
+
+
+// Dutch National Flag algorithm
+
 class Solution {
     public void sortColors(int[] nums) {
-        
-        int count0 = 0;
-        int count1 = 0;
+        int n = nums.length;
+        int low = 0, mid = 0, high = n - 1;
 
-        for(int num : nums) {
-            if(num == 0) {
-                count0++;
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                // Swap
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
             }
-            else if(num == 1) {
-                count1++;
-            }
-            
-        }
-
-        for(int i = 0; i < nums.length; i++) {
-            if(count0 != 0) {
-                nums[i] = 0;
-                count0--;
-            }
-            else if(count1 != 0) {
-                nums[i] = 1;
-                count1--;
+            else if(nums[mid] == 2) {
+                // swap
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
             }
             else{
-                nums[i] = 2;
+                mid++;
             }
         }
     }
