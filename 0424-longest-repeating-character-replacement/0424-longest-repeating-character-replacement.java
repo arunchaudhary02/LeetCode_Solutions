d698d53c765ffd0d8daa9b206561e56d5421ef2e
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-05-28 16:14:58 +0530

    Time: 28 ms (29.45%), Space: 45.1 MB (33.85%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,57 @@
+// Brute Force
+
+// class Solution {
+//     public int characterReplacement(String s, int k) {
+       
+//         int result = 0;
+//         for(int i = 0; i < s.length(); i++) {
+//             Map<Character, Integer> map = new HashMap<>();
+
+//             int maxFreq = 0;
+
+//             for(int j = i; j < s.length(); j++) {
+//                 map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+
+//                 maxFreq = Math.max(maxFreq, map.get(s.charAt(j)));
+
+//                 if( (j - i + 1) - maxFreq <= k) {
+//                     result = Math.max(result, j - i + 1);
+//                 }
+//             }
+//         }
+
+//         return result;
+        
+//     }
+// }
+
+
+// Sliding window
+
+
 class Solution {
     public int characterReplacement(String s, int k) {
-        int left = 0, right = 0, maxLen = 0, maxFrequency = 0;
-        int[] letterVisited = new int[26];
+       
+        Map<Character, Integer> map = new HashMap<>();
 
-        while(right < s.length()) {
-            char letter = s.charAt(right);
-            letterVisited[letter - 'A']++;
-            maxFrequency = Math.max(maxFrequency, letterVisited[letter - 'A']);
-            if( (right - left + 1) - maxFrequency > k) {
-                letterVisited[s.charAt(left) - 'A']--;
-                left++;
+        int result = 0;
+        int L = 0;
+        int maxFreq = 0;
+
+        for(int R = 0; R < s.length(); R++) {
+            map.put(s.charAt(R), map.getOrDefault(s.charAt(R), 0) + 1);
+
+            maxFreq = Math.max(maxFreq, map.get(s.charAt(R)));
+
+            while( (R - L + 1) - maxFreq > k) {
+                map.put(s.charAt(L), map.get(s.charAt(L)) - 1);
+                L++;
             }
 
-            if( (right - left + 1) - maxFrequency <= k) {
-                maxLen = Math.max(maxLen, right - left + 1);
-            }
-            right++;
+            result = Math.max(result, (R - L + 1));
         }
 
-        return maxLen;
+        return result;
+        
     }
 }
