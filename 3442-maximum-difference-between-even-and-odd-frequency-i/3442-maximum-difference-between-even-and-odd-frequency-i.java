6920c3799bbbbc4e466dd0dd46efecb7bf1bd465
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-06-10 08:56:09 +0530

    Time: 9 ms (5.49%), Space: 42.8 MB (40.05%) - LeetHub

--- 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.java
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.java
@@ -1,36 +1,21 @@
 class Solution {
     public int maxDifference(String s) {
-       
-        Map<Character, Integer> map = new HashMap<>();
-
-        for(char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        int[] arr = new int[26];
+        for(char c:s.toCharArray()){
+            arr[c-'a']++;
         }
-
-        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>( (a, b) -> b.getValue() - a.getValue());
-
-        pq.addAll(map.entrySet());
-
-        char a1 = ' ';
-        int tempA1 = 0;
-        char a2 = ' ';
-        int tempA2 = Integer.MAX_VALUE;
-
-        while(!pq.isEmpty()) {
-            Map.Entry<Character, Integer> entry = pq.poll();
-            if(entry.getValue() % 2 != 0 && tempA1 < entry.getValue() ) {
-                a1 = entry.getKey();
-                tempA1 = entry.getValue();
+        int odd =0;
+        int even = s.length();
+        for(int i:arr){
+            if(i%2==0 && i>0){
+                even =Math.min(even,i);
             }
-            else if(entry.getValue() % 2 == 0 && tempA2 > entry.getValue()) {
-                a2 = entry.getKey();
-                tempA2 = entry.getValue();
+            else {
+                odd= Math.max(odd,i);
             }
-            
         }
-
-        System.out.println(tempA1 + " " + a1);
-        System.out.println(tempA2 + " " + a2);
-        return (tempA1 - tempA2);
+        System.out.println(even+" "+odd);
+        return odd-even;
+        
     }
 }
