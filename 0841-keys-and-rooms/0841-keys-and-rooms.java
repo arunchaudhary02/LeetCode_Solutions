0d4bd6d64a0e0d85274ca3ff3f5ca5f3eb050919
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-06-10 12:13:42 +0530

    Time: 22 ms (5.2%), Space: 45.2 MB (9.9%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+
+        int n = rooms.size();
+        
+        return dfs(rooms, n);
+        
+
+    }
+
+    private boolean dfs(List<List<Integer>> adjList, int n) {
+
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[n];
+
+        for(Integer num : adjList.get(0)) {
+            stack.push(num);
+        }
+        visited[0] = true;
+
+        while(!stack.isEmpty()) {
+
+            int curr = stack.pop();
+            visited[curr] = true;
+
+            for(Integer neighbour : adjList.get(curr)) {
+                if(visited[neighbour] == false) {
+                    stack.push(neighbour);
+                    visited[neighbour] = true;
+                }
+            } 
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(i + " " + visited[i]);
+            if(visited[i] == false){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
