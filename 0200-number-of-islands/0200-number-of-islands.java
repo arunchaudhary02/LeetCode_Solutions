41a262abc7fdac467bfd5d9675fb936b0484438e
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-07-04 23:04:54 +0530

    Time: 51 ms (5.09%), Space: 55.2 MB (5.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,52 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        if(grid == null || grid.length == 0) return 0;
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Set<String> visited = new HashSet<>();
+        int countIslands = 0;
+
+        for(int r=0; r < rows; r++) {
+            for(int c=0; c < cols; c++) {
+                if(grid[r][c] == '1' && !visited.contains(r + "|" + c)){
+                    bfs(r, c, grid, visited);
+                    countIslands++;
                 }
             }
         }
-        
-        return numIslands;
+        return countIslands;
+
     }
-    
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
-            return;
+
+
+    public void bfs(int r, int c, char[][] grid, Set<String> visited) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {r, c});
+        visited.add(r + "|" + c);
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int row = current[0], col = current[1];
+
+            for(int[] direction : directions) {
+                int newRow = row + direction[0];
+                int newCol = col + direction[1];
+
+                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length) {
+
+                    if(grid[newRow][newCol] == '1' && !visited.contains(newRow + "|" + newCol)) {
+                        visited.add(newRow + "|" + newCol);
+                        queue.offer(new int[] {newRow, newCol});
+                    }
+
+                }
+            }
         }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
     }
+
+   
 }
