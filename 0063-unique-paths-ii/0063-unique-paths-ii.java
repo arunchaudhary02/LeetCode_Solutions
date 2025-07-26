63550aef6d7e8887c0b51aca6d6be9c1b9d2bfec
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-07-26 18:17:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,41 +1,34 @@
 class Solution {
+    Integer[][] memo;
+    int m;
+    int n;
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int row = obstacleGrid.length;
-        int col = obstacleGrid[0].length;
+        
+        m = obstacleGrid.length;
+        n = obstacleGrid[0].length;
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0;
 
-        // If the start or end cell is blocked, return 0
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[row - 1][col - 1] == 1) {
+        memo = new Integer[m][n];
+
+        return solve(obstacleGrid, 0, 0);
+    }
+
+    private int solve(int[][] obsGrid, int i, int j) {
+        if(i < 0 || i >= m || j < 0 || j >= n || obsGrid[i][j] == 1) {
             return 0;
         }
 
-        // DP array to store the number of unique paths
-        int[][] dp = new int[row][col];
-
-        // Initialize the start cell
-        dp[0][0] = 1;
-
-        // Fill the DP table
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                // Skip the obstacle cells
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-
-                // Add paths from the top cell
-                if (i > 0) {
-                    dp[i][j] += dp[i - 1][j];
-                }
-
-                // Add paths from the left cell
-                if (j > 0) {
-                    dp[i][j] += dp[i][j - 1];
-                }
-            }
+        if(memo[i][j] != null) {
+            return memo[i][j];
         }
 
-        // The answer is in the bottom-right corner
-        return dp[row - 1][col - 1];
+        if(i == m - 1 || j == n - 1) {
+            return 1;
+        }
+
+        int right = solve(obsGrid, i, j + 1);
+        int down = solve(obsGrid, i + 1, j);
+
+        return memo[i][j] = right + down;
     }
 }
