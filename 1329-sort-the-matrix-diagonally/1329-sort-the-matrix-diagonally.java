4976f8140d09c948daeca1897583da2af8b8e46b
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-08-11 09:52:29 +0530

    Time: 14 ms (11.42%), Space: 44.9 MB (57.02%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,41 +1,38 @@
 class Solution {
     int m;
     int n;
-    public int[][] diagonalSort(int[][] mat) {
-        
-        m = mat.length;
-        n = mat[0].length;
 
-        for(int j = 0; j < n; j++) {
-            sortedDiagonalList(mat, 0, j);
+    public int[][] diagonalSort(int[][] mat) {
+
+        int m = mat.length;
+        int n = mat[0].length;
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        // Group elements by diagonal (i - j)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                map.computeIfAbsent(i - j, k -> new ArrayList<>())
+                        .add(mat[i][j]);
+            }
         }
-        for(int i = 1; i < m; i++) {
-            sortedDiagonalList(mat, i, 0);
+
+        // Sort each diagonal's list
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            Collections.sort(entry.getValue());
+        }
+
+        // Fill back sorted values (from last to first for correct order)
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                List<Integer> list = map.get(i - j);
+                mat[i][j] = list.get(list.size() - 1); // last element
+                list.remove(list.size() - 1); // remove last
+            }
         }
 
         return mat;
+
     }
 
-    private void sortedDiagonalList(int[][] mat, int row, int col) {
-        List<Integer> list = new ArrayList<>();
-
-        int i = row, j = col;
-        while(i < m && j < n) {
-            list.add(mat[i][j]);
-            i++;
-            j++;
-        }
-
-        Collections.sort(list);
-        
-        i = row;
-        j = col;
-        int index = 0;
-        while(i < m && j < n) {
-            mat[i][j] = list.get(index);
-            index++;
-            i++;
-            j++;
-        }
-    }
 }
