35d3ec265809d6472ac61d6e6c5156bda5b4a251
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-08-20 13:30:00 +0530

    Time: 14 ms (24.5%), Space: 45.6 MB (40.48%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -1,4 +1,6 @@
 // Brute Force O(n^2) time complexity
+
+/*
 class Solution {
     public int bestClosingTime(String customers) {
         
@@ -28,5 +30,52 @@ class Solution {
         }
 
         return index;
+    }
+}
+*/
+
+
+// Optimal Using prefix and suffix
+
+class Solution {
+    public int bestClosingTime(String customers) {
+        
+        int n = customers.length();
+        int[] prefix = new int[n + 1];
+        prefix[0] = 0;
+
+        int index = 1;
+        for(int i = 0; i < n; i++) {
+            prefix[index] = prefix[index - 1];
+            if(customers.charAt(i) == 'N') {
+                prefix[index]++;
+            }
+            index++;
+        }
+
+        int[] suffix = new int[n + 1];
+        suffix[n] = 0;
+        index = n - 1;
+        for(int i = n - 1; i >= 0; i--) {
+            suffix[index] = suffix[index + 1];
+            if(customers.charAt(i) == 'Y') {
+                suffix[index]++;
+            }
+            index--;
+        }
+
+        int resultPenalty = Integer.MAX_VALUE;
+        int resultIndex = -1;
+
+        for(int i = 0; i <= n; i++) {
+            int currPenalty = prefix[i] + suffix[i];
+
+            if(currPenalty < resultPenalty) {
+                resultPenalty = currPenalty;
+                resultIndex = i;
+            }
+        }
+
+        return resultIndex;
     }
 }
