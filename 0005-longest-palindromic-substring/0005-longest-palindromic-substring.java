d254ef97e865f1f6d110a54066601875a88c12cf
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-12-23 10:17:09 +0530

    Time: 202 ms (21.42%), Space: 105.7 MB (5.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public String longestPalindrome(String s) {
         
@@ -61,10 +62,64 @@ class Solution {
     }
 }
 
+*/
 
 
-// class Solution {
-//     public String longestPalindrome(String s) {
+//Approach 1 - Recursion + Memoization
+//T.C : O(n^2) - Because the AMortized Time Complexity of solve() will become 1 due to memoization.
+//S.C : O(n^2)
+
+class Solution {
+
+    private int[][] memo;
+
+    public String longestPalindrome(String s) {
         
-//     }
-// }
+        int len = s.length();
+        int maxLen = Integer.MIN_VALUE;
+        int startIndex = 0;
+        memo = new int[len][len];
+
+        for(int i = 0; i < len; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < len; j++) {
+                if( j - i + 1 > maxLen && solve(s, i, j)) {
+                    startIndex = i;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+
+
+        return s.substring(startIndex, startIndex + maxLen);
+    }
+
+    private boolean solve(String s, int l, int r) {
+        if(l >= r) {
+            return true;
+        }
+        if(memo[l][r] != -1) {
+            return memo[l][r] == 1;
+        }
+
+        if(s.charAt(l) == s.charAt(r)) {
+            memo[l][r] = solve(s, l + 1, r - 1) ? 1 : 0;
+        }
+        else{
+            memo[l][r] = 0;
+        }
+
+        return memo[l][r] == 1;
+    }
+}
+
+
+
+
+
+
+
+
