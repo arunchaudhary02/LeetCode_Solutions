4bd5985aa5935c0ba5c11275e0781ebb61d0cda7
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-12-24 15:49:44 +0530

    Time: 28 ms (50.82%), Space: 83.1 MB (50.56%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,3 +1,6 @@
+// Quick Sort
+
+/*
 class Solution {
     public int[] sortArray(int[] nums) {
         quickSort(nums, 0, nums.length - 1);
@@ -36,5 +39,66 @@ class Solution {
 
         quickSort(nums, low, end);
         quickSort(nums, start, high);
+    }
+}
+
+*/
+
+
+// Merse Sort
+
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+
+
+        mergeSort(nums, 0, nums.length - 1);
+
+        return nums;
+    }
+
+    private void mergeSort(int[] arr, int left, int right) {
+        if(left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for(int i = 0; i < n1; i++) {
+            leftArr[i] = arr[left + i];
+        }
+
+        for(int i = 0; i < n2; i++) {
+            rightArr[i] = arr[mid + 1 + i];
+        }
+
+        int L = 0, R = 0, K = left;
+
+        while(L < n1 && R < n2) {
+            if(leftArr[L] < rightArr[R]) {
+                arr[K++] = leftArr[L++];
+            }
+            else{
+                arr[K++] = rightArr[R++];
+            }
+        }
+
+        while(L < n1) {
+            arr[K++] = leftArr[L++];
+        }
+
+        while(R < n2) {
+            arr[K++] = rightArr[R++];
+        }
     }
 }
