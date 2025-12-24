1e561f0f3b065f7af3aeac8d995fcece6be20759
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-12-24 14:25:04 +0530

    Time: 15 ms (61.59%), Space: 93.4 MB (42.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,66 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         
-        mergeSort(0, nums.length - 1, nums);
+        Set<Integer> set = new HashSet<>();
 
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i] == nums[i - 1]) {
+        for(int num : nums) {
+            if(set.contains(num)) {
                 return true;
             }
+            set.add(num);
         }
-
+       
         return false;
     }
 
-    private void mergeSort(int low, int high, int[] arr) {
-        if(low < high) {
-            int mid = low + (high - low) / 2;
-            mergeSort(low, mid, arr);
-            mergeSort(mid + 1, high, arr);
-            merge(low, mid, high, arr);
-        }
-    }
-
-    private void merge(int low, int mid, int high, int[] arr) {
-        int[] leftTemp = new int[mid - low + 1];
-        int[] rightTemp = new int[high - mid];
-
-        int j = 0;
-        for(int i = low; i <= mid; i++) {
-            leftTemp[j] = arr[i];
-            j++;
-        }
-
-        j = 0;
-        for(int i = mid + 1; i <= high; i++) {
-            rightTemp[j] = arr[i];
-            j++;
-        }
-
-        int L = 0, R = 0, k =  low;
-
-        while(L < leftTemp.length && R < rightTemp.length) {
-            if(leftTemp[L] < rightTemp[R]) {
-                arr[k] = leftTemp[L];
-                L++;
-            }
-            else{
-                arr[k] = rightTemp[R];
-                R++;
-            }
-            k++;
-        }
-
-        while(L < leftTemp.length) {
-            arr[k] = leftTemp[L];
-            L++;
-            k++;
-        }
-
-        while(R < rightTemp.length) {
-            arr[k] = rightTemp[R];
-            k++;
-            R++;
-        }
-    }
 }
