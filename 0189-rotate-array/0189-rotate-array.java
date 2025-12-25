1aa0ebe4586f98393aa1ca5f7572096be31edb41
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2025-12-25 14:46:04 +0530

    Time: 1 ms (46.28%), Space: 63.5 MB (6.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -54,26 +54,27 @@
 class Solution {
     public void rotate(int[] nums, int k) {
 
-        k = k % nums.length;
+        int len = nums.length;
+        k = k % len;
 
-        if(k == 0 || k == nums.length) {
+        if(k == 0 || k == len) {
             return;
         }
 
-        int[] temp = new int[nums.length];
+        int[] tempArr = new int[len];
 
         int index = 0;
 
-        for(int i = nums.length - k; i < nums.length; i++) {
-            temp[index++] = nums[i];
+        for(int i = len - k; i < len; i++) {
+            tempArr[index++] = nums[i];
         }
 
-        for(int i = 0; i < nums.length - k; i++) {
-            temp[index++] = nums[i];
+        for(int i = 0; i < len - k; i++) {
+            tempArr[index++] = nums[i];
         }
 
-        for(int i = 0; i < nums.length; i++) {
-            nums[i] = temp[i];
+        for(int i = 0; i < len; i++) {
+            nums[i] = tempArr[i];
         }
     }
 }
