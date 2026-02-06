35471bf4d44dd02f413c6cb74855dd55e43e1257
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-02-06 23:08:50 +0530

    Time: 3 ms (70.66%), Space: 47.9 MB (17.17%) - LeetHub

---
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
@@ -0,0 +1,18 @@
+class Solution {
+    private void helper(int n,String val,List<String> ans){
+        if(val.length()==n){
+            ans.add(val);
+            return;
+        }
+        helper(n,val+"1",ans);
+        if(val.charAt(val.length()-1)!='0'){
+            helper(n,val+"0",ans);
+        }
+    }
+    public List<String> validStrings(int n) {
+        List<String> ans=new ArrayList<>();
+        helper(n,"0",ans);
+        helper(n,"1",ans);
+        return ans;
+    }
+}
