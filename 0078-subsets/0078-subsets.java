f2ebe1dec2285d3a1f0bc0431404cb7a3de0330c
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-03-14 11:11:06 +0530

    Time: 1 ms (88.14%), Space: 44 MB (93.55%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,24 +1,47 @@
+
+// Method 1 // By Kunal
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> subSet = new ArrayList<>();
-
-        dfs(nums, 0, subSet, result);
-
-        return result;
-
-    }
-
-    private void dfs(int[] nums, int i, List<Integer> subSet, List<List<Integer>> result) {
-        if(i >= nums.length) {
-            result.add(new ArrayList<>(subSet));
-            return;
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for(int num : nums) {
+            int n = outer.size();
+            for(int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
         }
 
-        subSet.add(nums[i]);
-
-        dfs(nums, i + 1, subSet, result);
-        subSet.remove(subSet.size() - 1);
-        dfs(nums, i + 1, subSet, result);
+        return outer;
     }
 }
+
+
+// Method 2 By NeetCode  OR MIK
+
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         List<Integer> subSet = new ArrayList<>();
+
+//         dfs(nums, 0, subSet, result);
+
+//         return result;
+//     }
+
+//     private static void dfs(int[] nums, int index, List<Integer> subSet, List<List<Integer>> result) {
+
+
+//         if(index >= nums.length) {
+//             result.add(new ArrayList<>(subSet));
+//             return;
+//         }
+
+//         subSet.add(nums[index]);
+//         dfs(nums, index + 1, subSet, result);
+//         subSet.removeLast();
+//         dfs(nums, index + 1, subSet, result);
+//     }
+// }
