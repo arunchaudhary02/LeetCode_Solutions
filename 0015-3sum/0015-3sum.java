f6829d5938bd2ce9a1958a589b4c2645f557c15b
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-04-02 11:59:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            
-            int j = i + 1;
-            int k = nums.length - 1;
 
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
+            int L = i + 1;
+            int R = nums.length - 1;
 
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
+            while(L < R) {
+                int total = nums[i] + nums[L] + nums[R];
+                if(total > 0) {
+                    R--;
+                }
+                else if(total < 0) {
+                    L++;
+                }
+                else{
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[L]);
+                    temp.add(nums[R]);
+                    result.add(temp);
 
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
+                    L++;
+                    while(nums[L] == nums[L - 1] && L < R) {
+                        L++;
                     }
                 }
             }
-        }
-        return res;        
+
+            
+        }  
+
+        return result;  
     }
 }
 
@@ -70,6 +79,7 @@ class Solution {
 //         return result;
 //     }
 // }
+
 
 
 
