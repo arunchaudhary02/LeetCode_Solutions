589e2dd135975c1fc0cb0b16ef729453d586dd97
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-04-14 12:27:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,28 +1,7 @@
-// Optimla Approach
+
+// Approach 1 Recursion + DP
 
 /*
-class Solution {
-    public int rob(int[] nums) {
-        int prev = nums[0];
-        int prev2 = 0;
-        int curr = 0;
-
-        for(int i = 1; i < nums.length; i++) {
-            int take = nums[i] + prev2;
-            int notTake = prev;
-
-            curr = Math.max(take, notTake);
-            prev2 = prev;
-            prev = curr;
-        }
-
-        return prev;
-    }
-
-}
-
-*/
-
 class Solution {
 
     int n;
@@ -56,3 +35,61 @@ class Solution {
     }
 
 }
+
+*/
+
+
+// Approach 2 bottom Up
+class Solution {
+
+    int n;
+    public int rob(int[] nums) {
+        n = nums.length;
+
+        if(n == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 2; i <= n; i++) {
+
+            int take = nums[i - 1] + dp[i - 2];
+            int notTake = dp[i - 1];
+
+            dp[i] = Math.max(take, notTake);
+        }
+
+        return dp[n];
+
+    }
+}
+
+
+// Optimla Approach
+
+/*
+class Solution {
+    public int rob(int[] nums) {
+        int prev = nums[0];
+        int prev2 = 0;
+        int curr = 0;
+
+        for(int i = 1; i < nums.length; i++) {
+            int take = nums[i] + prev2;
+            int notTake = prev;
+
+            curr = Math.max(take, notTake);
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+    }
+
+}
+
+*/
