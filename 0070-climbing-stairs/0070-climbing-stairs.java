c12eec1cb831a85ed472fbc424f1b70ea13e03cc
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-04-14 10:30:49 +0530

    Time: 0 ms (100%), Space: 42 MB (76.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,22 +1,107 @@
+
+// Brute Force
+
+// class Solution {
+//     public int climbStairs(int n) {
+        
+//         return solve(n);
+
+//     }
+
+//     private int solve(int n) {
+//         if(n < 0) {
+//             return 0;
+//         }
+//         if(n == 0) {
+//             return 1;
+//         }
+
+//         int leftOneStep = climbStairs(n - 1);
+//         int rightTwoStep = climbStairs(n - 2);
+
+//         return leftOneStep + rightTwoStep;
+//     }
+// }
+
+// Memorization 
+
+// class Solution {
+//     public int climbStairs(int n) {
+//         Map<Integer, Integer> memo = new HashMap<>();
+//         return solve(n, memo);
+
+//     }
+
+//     private int solve(int n, Map<Integer, Integer> memo) {
+//         if(n < 0) {
+//             return 0;
+//         }
+
+//         if(memo.containsKey(n)) {
+//             return memo.get(n);
+//         }
+//         if(n == 0) {
+//             return 1; // we got 1 way to cover all the stairs
+//         }
+
+//         int leftOneStep = climbStairs(n - 1);
+//         int rightTwoStep = climbStairs(n - 2);
+
+//         int ways = leftOneStep + rightTwoStep;
+//         memo.put(n, ways);
+//         return ways;
+//     }
+// }
+
+
+//Approach-2 (Using Bottom Up DP)
+
+
+// class Solution {
+//     public int climbStairs(int n) {
+
+//         if(n == 0 || n == 1 || n == 2) {
+//             return n;
+//         }
+        
+//         int[] dp = new int[n + 1]; // indexing starting from 1
+        
+//         dp[1] = 1;
+//         dp[2] = 2;
+
+//         for(int i = 3; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+
+//         return dp[n];
+
+//     }
+// }
+
+
+//Approach-3 (Improving approach-2) -
+//See that it looks nothing but a fibonacci sequence. You just have to store last 2 values (t[i-1] & t[i-2]) to find current value (t[i])
+
 class Solution {
     public int climbStairs(int n) {
-        Map<Integer, Integer> memo = new HashMap<>();
 
-        return climStairs(n, memo);
-    }
+        if(n == 0 || n == 1 || n == 2) {
+            return n;
+        }
+        
+        int a = 1;
+        int b = 2;
+        int next = 0;
 
-    private int climStairs(int n, Map<Integer, Integer> memo) {
-        if(n == 0 || n == 1) {
-            return 1;
+        for(int i = 3; i <= n; i++) {
+            next = a + b;
+            a = b;
+            b = next;
         }
 
-        if(memo.containsKey(n)) {
-            return memo.get(n);
-        }
+        return next;
 
-        int ways = climStairs(n - 1, memo) + climStairs(n - 2, memo);
-        memo.put(n, ways);
-
-        return ways;
     }
 }
+
+
