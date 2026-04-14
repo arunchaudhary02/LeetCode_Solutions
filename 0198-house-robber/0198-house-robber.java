212a45e3a4f9cb625b1c7c27f4facaad1ca851e5
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-04-14 12:13:59 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,3 +1,6 @@
+// Optimla Approach
+
+/*
 class Solution {
     public int rob(int[] nums) {
         int prev = nums[0];
@@ -14,6 +17,42 @@ class Solution {
         }
 
         return prev;
+    }
+
+}
+
+*/
+
+class Solution {
+
+    int n;
+    public int rob(int[] nums) {
+        n = nums.length;
+
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, -1);
+
+        return recursion(0, nums, dp);
+    }
+
+    private int recursion(int index, int[] nums, int[] dp) {
+
+        if(n <= index) {
+            return 0;
+        }
+
+        if(dp[index] != -1) {
+            return dp[index];
+        }
+
+        int take = recursion(index + 2, nums, dp) + nums[index];
+        int notTake = recursion(index + 1, nums, dp);
+
+        dp[index] = Math.max(take, notTake);
+
+        return dp[index];
+        
     }
 
 }
