43b7c5d5531f40cb16f323bcf0da33de34481816
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-04-19 11:38:03 +0530

    Time: 0 ms (100%), Space: 43.2 MB (81.13%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,3 +1,5 @@
+// Approach 1 Best 
+
 class Solution {
     public int[] plusOne(int[] digits) {
         
@@ -13,6 +15,7 @@ class Solution {
             digits[i] = 0;
         }
 
+        // If all digits were 9 (like 999 -> 1000)
         int[] result = new int[digits.length + 1];
        
         result[0] = 1;
@@ -20,3 +23,40 @@ class Solution {
 
     }
 }
+
+// Approach 2 
+
+/*
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+        boolean isOverflow = false;
+        int n = digits.length;
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(digits[i] == 9) {
+                isOverflow = true;
+                digits[i] = 0;
+            }
+            else{
+                isOverflow = false;
+                digits[i] += 1;
+                break;
+            }
+        }
+
+        if(isOverflow) {
+            int[] result = new int[n + 1]; 
+            result[0] = 1;
+            for(int i = 1; i < n + 1; i++) {
+                result[i] = digits[i - 1];
+            }
+            
+
+            return result;
+        }
+
+        return digits;
+    }
+}
+*/
