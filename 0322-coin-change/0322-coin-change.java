eb1f19807530b20cddc13e42f940d785c6dc5da3
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-05-16 10:27:35 +0530

    Time: 16 ms (64.25%), Space: 46.2 MB (70.83%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -32,6 +32,7 @@ class Solution {
 
 // DP by HashMap memo
 
+/*
 class Solution {
 
     Map<String, Integer> memo = new HashMap<>();
@@ -73,4 +74,36 @@ class Solution {
 
         return result;
     }
+}
+*/
+
+// DP Bottom Up Approach https://www.youtube.com/watch?v=NNcN5X1wsaw
+
+class Solution {
+
+    public int coinChange(int[] coins, int amount) {
+        
+        if(amount < 1) {
+            return 0;
+        }
+
+        int[] minCoinsDp = new int[amount + 1];
+
+        for(int i = 1; i <= amount; i++) {
+
+            minCoinsDp[i] = Integer.MAX_VALUE;
+
+            for(int coin : coins) {
+                if(coin <= i && minCoinsDp[i - coin] != Integer.MAX_VALUE) {
+                    minCoinsDp[i] = Math.min(minCoinsDp[i], 1 + minCoinsDp[i - coin]);
+                }
+            }
+        }
+
+        if(minCoinsDp[amount] == Integer.MAX_VALUE ) return -1;
+
+        return minCoinsDp[amount];
+    }
+
+    
 }
