2bf4e08f5ec055f5987ea90a412edf303fed2308
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-07-09 23:06:38 +0530

    Time: 15 ms (42.8%), Space: 48.3 MB (96.49%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,39 +2,36 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int rows = mat.length;
         int cols = mat[0].length;
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int[][] directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
         Queue<int[]> queue = new LinkedList<>();
 
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 if(mat[i][j] == 0) {
-                    queue.add(new int[] {i , j});
+                    queue.offer(new int[]{i, j});
                 }
                 else {
                     mat[i][j] = Integer.MAX_VALUE;
-                }
+                }   
             }
         }
 
         while(!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            int row = cell[0];
-            int col = cell[1];
+            int[] currNode = queue.poll();
+            int row = currNode[0];
+            int col = currNode[1];
 
             for(int[] direction : directions) {
                 int newRow = row + direction[0];
                 int newCol = col + direction[1];
 
-                if(newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && mat[newRow][newCol] > mat[row][col] + 1){ 
-                    mat[newRow][newCol] = mat[row][col] + 1;
-                    queue.offer(new int[] {newRow, newCol});
+                if(0 <= newRow && newRow < rows && 0 <= newCol && newCol < cols && mat[row][col] + 1 < mat[newRow][newCol]) {
+                    mat[newRow][newCol] = 1 + mat[row][col];
+                    queue.offer(new int[]{newRow, newCol});
                 }
             }
         }
 
         return mat;
-      
     }
-
-   
 }
