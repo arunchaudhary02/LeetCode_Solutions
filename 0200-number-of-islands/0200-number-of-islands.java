dcb3b30bebdc74cf4048d1904c0dda2d12746907
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-07-10 12:09:29 +0530

    Time: 4 ms (46.58%), Space: 52.2 MB (68.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,52 +1,34 @@
 class Solution {
+    int rows;
+    int cols;
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
+        rows = grid.length;
+        cols = grid[0].length;
 
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Set<String> visited = new HashSet<>();
-        int countIslands = 0;
+        boolean[][] visited = new boolean[rows][cols];
+        int count = 0;
 
-        for(int r=0; r < rows; r++) {
-            for(int c=0; c < cols; c++) {
-                if(grid[r][c] == '1' && !visited.contains(r + "|" + c)){
-                    bfs(r, c, grid, visited);
-                    countIslands++;
-                }
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                if(visited[r][c] == false && grid[r][c] == '1') {
+                    count++;
+                    dfsHelper(r, c, visited, grid);
+                }   
             }
         }
-        return countIslands;
 
+        return count;
     }
 
-
-    public void bfs(int r, int c, char[][] grid, Set<String> visited) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {r, c});
-        visited.add(r + "|" + c);
-
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-
-        while(!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int row = current[0], col = current[1];
-
-            for(int[] direction : directions) {
-                int newRow = row + direction[0];
-                int newCol = col + direction[1];
-
-                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length) {
-
-                    if(grid[newRow][newCol] == '1' && !visited.contains(newRow + "|" + newCol)) {
-                        visited.add(newRow + "|" + newCol);
-                        queue.offer(new int[] {newRow, newCol});
-                    }
-
-                }
-            }
+    private void dfsHelper(int r, int c, boolean[][] visited, char[][] grid) {
+        if(r < 0 || rows <= r || c < 0 || cols <= c || visited[r][c] == true || grid[r][c] == '0') {
+            return;
         }
-    }
 
-   
+        visited[r][c] = true;
+        dfsHelper(r + 1, c, visited, grid);
+        dfsHelper(r, c + 1, visited, grid);
+        dfsHelper(r - 1, c, visited, grid);
+        dfsHelper(r, c - 1, visited, grid);
+    }
 }
