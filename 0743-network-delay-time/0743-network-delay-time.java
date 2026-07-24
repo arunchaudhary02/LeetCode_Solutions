c2ae069fd6b6497e87aad5c289560b65778594a4
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-07-24 13:25:47 +0530

    Time: 10 ms (80.96%), Space: 48.7 MB (85.95%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,44 +1,55 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<int[]>> adjList = new ArrayList<>();
 
-        Map<Integer, List<int[]>> adjList = new HashMap<>();
+        for(int i = 0; i <= n; i++) {
+            adjList.add(new ArrayList<>());
+        }
 
-        for (int[] time : times) {
-            int u = time[0];
-            int v = time[1];
-            int w = time[2];
-            adjList.computeIfAbsent(u, key -> new ArrayList<>()).add(new int[] { v, w });
+        for(int[] time : times) {
+            int src = time[0];
+            int dest = time[1];
+            int weight = time[2];
+
+            adjList.get(src).add(new int[] {dest, weight});
         }
 
         int[] distance = new int[n + 1];
         Arrays.fill(distance, Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        pq.offer(new int[] {0, k});
         distance[k] = 0;
 
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]); // [time, node]
-        minHeap.offer(new int[] { 0, k });
+        while(!pq.isEmpty()) {
+            int[] currNode = pq.poll();
 
-        while (!minHeap.isEmpty()) {
-            int[] current = minHeap.poll();
-            int currTime = current[0];
-            int currNode = current[1];
+            int node = currNode[1];
+            int d = currNode[0];
 
-            for (int[] neighbour : adjList.getOrDefault(currNode, Collections.emptyList())) {
-                int nextNode = neighbour[0];
-                int nextTime = neighbour[1];
+            if(distance[node] < d) continue;
 
-                if (currTime + nextTime < distance[nextNode]) {
-                    distance[nextNode] = currTime + nextTime;
-                    minHeap.offer(new int[] { currTime + nextTime, nextNode });
+            for(int[] neighbour : adjList.get(node)) {
+                int adjNode = neighbour[0];
+                int weight = neighbour[1];
+
+                if(d + weight < distance[adjNode]) {
+                    distance[adjNode] = d + weight;
+                    pq.offer(new int[] {d + weight, adjNode});
                 }
             }
         }
 
-        int result = 0;
+        int resultTime = -1;
+
         for (int i = 1; i <= n; i++) {
-            if (distance[i] == Integer.MAX_VALUE) return -1;
-            result = Math.max(result, distance[i]);
+            if (distance[i] == Integer.MAX_VALUE) {
+                return -1;
+            }
+
+            resultTime = Math.max(resultTime, distance[i]);
         }
 
-        return result;
+        return resultTime;
     }
 }
