39009958aa813a9a5397c71d529cbe66593fdec4
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-09-03 13:17:20 +0530

    Time: 44 ms (10.43%), Space: 47.2 MB (8.82%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,41 +1,101 @@
 class Solution {
-   public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] distance = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(distance[i], 1000000000); // Use a large value as infinity
-            distance[i][i] = 0;
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        // int[][] shortestPaths = new int[n][n];
+
+        // for(int[] edge : edges) {
+        //     int i = edge[0];
+        //     int j = edge[1];
+        //     int wt = edge[2];
+
+        //     shortestPaths[i][j] = wt;
+        //     shortestPaths[j][i] = wt;
+        // }
+
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j < n; j++) {
+        //         if(i == j) {
+        //             shortestPaths[i][j] = 0;
+        //         }
+        //         else if(shortestPaths[i][j] == 0) {
+        //             shortestPaths[i][j] = Integer.MAX_VALUE;
+        //         }
+
+        //     }
+        // }
+
+        List<List<int[]>> adjList = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
         }
 
-        for (int[] edge : edges) {
-            distance[edge[0]][edge[1]] = edge[2];
-            distance[edge[1]][edge[0]] = edge[2];
+        for(int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            adjList.get(u).add(new int[] {v, wt});
+            adjList.get(v).add(new int[] {u, wt});
         }
 
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+        int[][] shortestPaths = new int[n][n];
+
+        for(int src = 0; src < n; src++) {
+            dijkstraSolve(src, adjList, shortestPaths[src]);
+            System.out.println(Arrays.toString(shortestPaths[src]));
+        }
+
+        int resultCity = 0;
+        int countCity = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(shortestPaths[0][i] != Integer.MAX_VALUE && shortestPaths[0][i] != 0 && shortestPaths[0][i] <= distanceThreshold) {
+                countCity++;
+            }
+        }
+
+        for(int i = 1; i < n; i++) {
+            int countCurrent = 0;
+            for(int j = 0; j < n; j++) {
+                if(shortestPaths[i][j] != Integer.MAX_VALUE && shortestPaths[i][j] != 0 && shortestPaths[i][j] <= distanceThreshold) {
+                    countCurrent++;
+                }
+            }
+
+            if(countCurrent <= countCity) {
+                resultCity = i;
+                countCity = countCurrent;
+            }
+        }
+
+        return resultCity;
+    }
+
+    private void dijkstraSolve(int src, List<List<int[]>> adjList, int[] distance) {
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[src] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        pq.offer(new int[] {0, src});
+
+        while(!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int wt = curr[0];
+            int node = curr[1];
+
+            if(distance[node] < wt) {
+                continue;
+            }
+
+            for(int[] neighbour : adjList.get(node)) {
+                int nextNode = neighbour[0];
+                int nextWt = neighbour[1];
+
+                if(wt + nextWt < distance[nextNode]) {
+                    distance[nextNode] = wt + nextWt;
+                    pq.offer(new int[] {wt + nextWt, nextNode});
                 }
             }
         }
-
-        int ans = -1;
-        int mini = Integer.MAX_VALUE;
-        int[] reachable = new int[n];
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (i != j && distance[i][j] <= distanceThreshold) {
-                    count++;
-                }
-            }
-            reachable[i] = count;
-            if (count <= mini) {
-                mini = count;
-                ans = i;
-            }
-        }
-
-        return ans;
     }
 }
