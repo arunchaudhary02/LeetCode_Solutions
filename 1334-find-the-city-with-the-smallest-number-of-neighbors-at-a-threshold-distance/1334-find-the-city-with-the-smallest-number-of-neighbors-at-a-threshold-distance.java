479559c1162ec77ff669d479ee1c7f7e9d2ddee4
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-09-05 22:52:15 +0530

    Time: 10 ms (67.19%), Space: 46.3 MB (84.48%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -4,28 +4,6 @@
 
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        // int[][] shortestPaths = new int[n][n];
-
-        // for(int[] edge : edges) {
-        //     int i = edge[0];
-        //     int j = edge[1];
-        //     int wt = edge[2];
-
-        //     shortestPaths[i][j] = wt;
-        //     shortestPaths[j][i] = wt;
-        // }
-
-        // for(int i = 0; i < n; i++) {
-        //     for(int j = 0; j < n; j++) {
-        //         if(i == j) {
-        //             shortestPaths[i][j] = 0;
-        //         }
-        //         else if(shortestPaths[i][j] == 0) {
-        //             shortestPaths[i][j] = Integer.MAX_VALUE;
-        //         }
-
-        //     }
-        // }
 
         List<List<int[]>> adjList = new ArrayList<>();
 
@@ -106,6 +84,9 @@ class Solution {
 
 // Using Bellman Ford
 
+/*
+
+
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
         int[][] shortestPathMatrix = new int[n][n];
@@ -128,18 +109,82 @@ class Solution {
                 int wt = edge[2];
 
                 // u -> v
-                if(distance[u] != Integer.MAX_VALUE &&
-                distance[u] + wt < distance[v]) {
+                if(distance[u] != Integer.MAX_VALUE && distance[u] + wt < distance[v]) {
                     distance[v] = distance[u] + wt;
                 }
 
-                // v -> u (missing in your code)
-                if(distance[v] != Integer.MAX_VALUE &&
-                distance[v] + wt < distance[u]) {
+                // v -> u 
+                if(distance[v] != Integer.MAX_VALUE && distance[v] + wt < distance[u]) {
                     distance[u] = distance[v] + wt;
                 }
             }
         }
+    }
+
+    private int getCityWithFewestReachable(int n, int[][] shortestPathMatrix, int distanceThreshold) {
+        int cityWithFewestReachable = -1;
+        int fewestReachableCount = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            int countRechable = 0;
+
+            for(int j = 0; j < n; j++) {
+                if(i != j && shortestPathMatrix[i][j] <= distanceThreshold) {
+                    countRechable++;
+                }
+            }
+
+            if(countRechable <= fewestReachableCount) {
+                fewestReachableCount = countRechable;
+                cityWithFewestReachable = i;
+            }
+        }
+
+        return cityWithFewestReachable;
+    }
+}
+
+*/
+
+
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] shortestPaths = new int[n][n];
+
+        for(int[] edge : edges) {
+            int i = edge[0];
+            int j = edge[1];
+            int wt = edge[2];
+
+            shortestPaths[i][j] = wt;
+            shortestPaths[j][i] = wt;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j) {
+                    shortestPaths[i][j] = 0;
+                }
+                else if(shortestPaths[i][j] == 0) {
+                    shortestPaths[i][j] = Integer.MAX_VALUE;
+                }
+
+            }
+        }
+
+        for(int via = 0; via < n; via++) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(shortestPaths[i][via] == Integer.MAX_VALUE || shortestPaths[via][j] == Integer.MAX_VALUE) {
+                        continue;
+                    }
+
+                    shortestPaths[i][j] = Math.min(shortestPaths[i][j], shortestPaths[i][via] + shortestPaths[via][j]);
+                }
+            }
+        }
+
+        return getCityWithFewestReachable(n, shortestPaths, distanceThreshold);
     }
 
     private int getCityWithFewestReachable(int n, int[][] shortestPathMatrix, int distanceThreshold) {
