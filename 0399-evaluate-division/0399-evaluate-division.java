569a73eb80b314cb5b11e4f63c6188236609719a
arunchaudhary02 <157296288+arunchaudhary02@users.noreply.github.com>
2026-09-10 10:58:03 +0530

    Time: 2 ms (35.44%), Space: 46.8 MB (78.33%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,5 +1,7 @@
-class Solution {
+// Using DFS
 
+/*
+class Solution {
     static class Pair {
         String node;
         double value;
@@ -32,7 +34,7 @@ class Solution {
 
             double[] ans = {-1.0};
 
-            if(adj.containsKey(src)) {
+            if(adj.containsKey(src) && adj.containsKey(dst)) {
                 Set<String> visited = new HashSet<>();
                 dfs(src, dst, visited, adj, 1.0, ans);
             }
@@ -58,5 +60,80 @@ class Solution {
         for(Pair neighbour : adj.get(src)) {
             dfs(neighbour.node, dst, visited, adj, product * neighbour.value, ans);
         }
+    }
+}
+
+*/
+
+// Using DFS
+
+class Solution {
+    static class Pair {
+        String node;
+        double value;
+
+        Pair(String node, double value) {
+            this.node = node;
+            this.value = value;
+        }
+    }
+    
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, List<Pair>> adj = new HashMap<>();
+
+        for(int i = 0; i < equations.size(); i++) {
+            String u = equations.get(i).get(0);
+            String v = equations.get(i).get(1);
+            double wt = values[i];
+
+            adj.putIfAbsent(u, new ArrayList<>());
+            adj.putIfAbsent(v, new ArrayList<>());
+
+            adj.get(u).add(new Pair(v, wt));
+            adj.get(v).add(new Pair(u, 1 / wt));
+        }
+
+        double[] result = new double[queries.size()];
+
+        for(int i = 0; i < queries.size(); i++) {
+            String src = queries.get(i).get(0);
+            String dst = queries.get(i).get(1);
+
+            if(adj.containsKey(src) && adj.containsKey(dst)) {
+                result[i] = bfs(src, dst, adj);
+            }
+            else {
+                result[i] = -1.0;
+            }
+        }
+
+        return result;
+    }
+
+    private double bfs(String src, String dst, Map<String, List<Pair>> adj) {
+        Set<String> visited = new HashSet<>();
+        Queue<Pair> queue = new LinkedList<>(); // currNode, weight
+        queue.offer(new Pair(src, 1.0));
+        visited.add(src);
+
+        while(!queue.isEmpty()) {
+            Pair currNode = queue.poll();
+
+            if(currNode.node.equals(dst)) {
+                return currNode.value;
+            }
+
+            for(Pair neighbour : adj.get(currNode.node)) {
+                String nextNode = neighbour.node;
+                double nextWt = neighbour.value;
+
+                if(!visited.contains(nextNode)) {
+                    queue.offer(new Pair(nextNode, currNode.value * nextWt));
+                    visited.add(nextNode);
+                }
+            }
+        }
+
+        return -1.0;
     }
 }
